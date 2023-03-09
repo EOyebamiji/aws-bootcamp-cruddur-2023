70a4191e6787c3cbe91911db2a1858c6f6d057b7
Emmanuel Oyèbámijí <Emmanueloluwatosin56@gmail.com>
2023-03-09 12:53:42 +0000

    Added cognito for backend

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -88,8 +88,8 @@ origins = [frontend, backend]
 cors = CORS(
   app, 
   resources={r"/api/*": {"origins": origins}},
-  expose_headers="location,link",
-  allow_headers="content-type,if-modified-since",
+  headers=['Content-Type', 'Authorization'], 
+  expose_headers='Authorization',
   methods="OPTIONS,GET,HEAD,POST"
 )
 
@@ -162,6 +162,10 @@ def data_create_message():
 
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
+  app.logger.debug("AUTH HEADER")
+  app.logger.debug(
+    request.headers.get('Authorization')
+  )
   data = HomeActivities.run()
   return data, 200
 
