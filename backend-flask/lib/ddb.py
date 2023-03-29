74c1476e939ea329070560f481bcdf2cdc84d047
Emmanuel Oyèbámijí <Emmanueloluwatosin56@gmail.com>
2023-03-29 07:57:57 +0000

    implement conversations with dynamodb

--- backend-flask/lib/ddb.py
+++ backend-flask/lib/ddb.py
@@ -35,7 +35,8 @@ class Ddb:
     # query the table
     response = client.query(**query_params)
     items = response['Items']
-    
+    print ("items::", items)
+
     results = []
     for item in items:
       last_sent_at = item['sk']['S']
@@ -50,7 +51,7 @@ class Ddb:
 
   def list_messages(client,message_group_uuid):
     year = str(datetime.now().year)
-    table_name = 'cruddur-messages'
+    table_name = 'cruddur_messages'
     query_params = {
       'TableName': table_name,
       'KeyConditionExpression': 'pk = :pkey AND begins_with(sk, :year)',  #:sk=:skey, :pk=:pkey,
@@ -64,7 +65,7 @@ class Ddb:
 
     response = client.query(**query_params)
     items = response['Items']
-    
+    items.reverse()    
     results = []
     for item in items:
       created_at = item['sk']['S']
@@ -76,3 +77,34 @@ class Ddb:
         'created_at': created_at
       })
     return results
+
+def create_message(client,message_group_uuid, message, my_user_uuid, my_user_display_name, my_user_handle):
+  now = datetime.now(timezone.utc).astimezone().isoformat()
+  created_at = now
+  message_uuid = str(uuid.uuid4())
+
+  record = {
+      'pk':   {'S': f"MSG#{message_group_uuid}"},
+      'sk':   {'S': created_at },
+      'message': {'S': message},
+      'message_uuid': {'S': message_uuid},
+      'user_uuid': {'S': my_user_uuid},
+      'user_display_name': {'S': my_user_display_name},
+      'user_handle': {'S': my_user_handle}
+    }
+    # insert the record into the table
+  table_name = 'cruddur_messages'
+  response = client.put_item(
+      TableName=table_name,
+      Item=record
+    )
+    # print the response
+  print(response)
+  return {
+      'message_group_uuid': message_group_uuid,
+      'uuid': my_user_uuid,
+      'display_name': my_user_display_name,
+      'handle':  my_user_handle,
+      'message': message,
+      'created_at': created_at
+    }
