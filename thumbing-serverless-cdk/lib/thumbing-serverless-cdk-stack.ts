c2d4ec3395e9ad3c74327624f1c331fd7c55cbcb
Emmanuel Oyèbámijí <Emmanueloluwatosin56@gmail.com>
2023-05-26 08:10:07 +0000

    Week-8 | Implementing Serverless Image Process CDK

--- thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
+++ thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
@@ -1,6 +1,9 @@
 import * as cdk from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as s3n from 'aws-cdk-lib/aws-s3-notifications';
+import * as subscriptions from 'aws-cdk-lib/aws-sns-subscriptions';
+import * as sns from 'aws-cdk-lib/aws-sns';
 import { Construct } from 'constructs';
 import * as dotenv from 'dotenv';
 
@@ -28,6 +31,8 @@ export class ThumbingServerlessCdkStack extends cdk.Stack {
     const bucket = this.createBucket(bucketName); 
     const lambda = this.createLambda(functionPath, bucketName, folderInput, folderOutput);
 
+    this.createS3NotifyToLambda(folderInput,lambda,bucket)
+
   }
 
   createBucket(bucketName: string): s3.IBucket {
@@ -52,5 +57,14 @@ export class ThumbingServerlessCdkStack extends cdk.Stack {
       }
     });
     return lambdaFunction
+  }
+
+  createS3NotifyToLambda(prefix: string, lambda: lambda.IFunction, bucket: s3.IBucket): void {
+    const destination = new s3n.LambdaDestination(lambda);
+    bucket.addEventNotification(
+      s3.EventType.OBJECT_CREATED_PUT,
+      destination,
+      {prefix: prefix}
+    )
   }
 }
