2882a451906e57af3754cc315a5ebf53f269dabd
Emmanuel Oyèbámijí <Emmanueloluwatosin56@gmail.com>
2023-07-21 11:38:54 +0000

    Week X | Fixing Database Issue Prod

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -264,7 +264,7 @@ def data_search():
 @app.route("/api/activities", methods=['POST','OPTIONS'])
 @cross_origin()
 def data_activities():
-  user_handle  = {"/@" + props.user.handle}
+  user_handle  = '@EOyebamiji'
   message = request.json['message']
   ttl = request.json['ttl']
   model = CreateActivity.run(message, user_handle, ttl)
